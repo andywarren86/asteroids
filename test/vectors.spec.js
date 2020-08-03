b6a96c9aee58eea25ece0f6593f27c79f5f60954
Andrew <andywarren86@gmail.com>
2020-08-03 22:41:03 +1000

    Rework vector utils and add tests

--- test/vectors.spec.js
+++ test/vectors.spec.js
@@ -1,9 +1,46 @@
-const createVector = require('../vectors');
+const Vector = require('../vectors').default;
+// import Vectors from '../vectors';
 
-test('creates vector', () => {
-  console.log(createVector);
-  const v = createVector( 0, 3 );
+console.log("Vector", Vector);
 
-  expect(v.x).toBe(3);
-  expect(v.y).toBe(0);
-});
+describe("vector.js", () => {
+  test('creates new vector', () => {
+    const v = new Vector(1, 2);
+    expect(v.x).toBe(1);
+    expect(v.y).toBe(2);
+  });
+
+  test('fromAngle()', () => {
+    const v1 = Vector.fromAngle( 45, Math.sqrt(2) );
+    expect(v1.x).toBeCloseTo( 1 );
+    expect(v1.y).toBeCloseTo( 1 );
+
+    const v2 = Vector.fromAngle( 30, 2 );
+    expect( v2.x ).toBeCloseTo( Math.sqrt(3) );
+    expect( v2.y ).toBeCloseTo( 1 );
+  })
+
+  test('Vector.add()', () => {
+    const v1 = new Vector(1, 2);
+    const v2 = new Vector(2, 3);
+    const v3 = Vector.add(v1, v2);
+    expect(v3.x).toBe(3);
+    expect(v3.y).toBe(5);
+  })
+
+  test('mag()', () => {
+    expect(new Vector(3, 4).mag()).toBe(5);
+  });
+
+  test('angle()', () => {
+    expect(new Vector(1, 0).angle()).toBe(0);
+    expect(new Vector(1, 1).angle()).toBe(45);
+    expect(new Vector(0, 1).angle()).toBe(90);
+    expect(new Vector(-1, 1).angle()).toBe(135);
+    expect(new Vector(-1, 0).angle()).toBe(180);
+    expect(new Vector(-1, -1).angle()).toBe(225);
+    expect(new Vector(0, -1).angle()).toBe(270);
+    expect(new Vector(1, -1).angle()).toBe(315);
+  })
+})
+
--- vectors.js
+++ vectors.js
@@ -1,29 +1,39 @@
-export function createVector( angle, magnitude ) {
-  const x = Math.sin(angle * Math.PI / 180) * magnitude;
-  const y = -Math.cos(angle * Math.PI / 180) * magnitude;
-  return { x, y };
-}
+const toDegs = (rads) => rads * 180 / Math.PI;
+const toRads = (degs) => degs * Math.PI / 180;
 
-export function addVectors( v1, v2 ) {
-  return {
-    x: v1.x + v2.x,
-    y: v1.y + v2.y
+export default class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  mag() {
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  }
+
+  /** Angle in degrees */
+  angle() {
+    const deg = toDegs(Math.atan(this.y / this.x));
+
+    // 2nd / 3rd quadrants
+    if (this.x < 0) {
+      return deg + 180;
+    }
+    // 4th quadrant
+    else if (this.x >= 0 && this.y < 0) {
+      return deg + 360;
+    }
+    return deg;
   }
 }
 
-export function magnitude( v ) {
-  return Math.sqrt( Math.pow( v.x, 2 ) + Math.pow( v.y, 2 ) );
+Vector.fromAngle = (angle, magnitude) => {
+  const x = Math.cos(toRads(angle)) * magnitude;
+  const y = Math.sin(toRads(angle)) * magnitude;
+  return new Vector(x, y);
 }
 
-/** Angle in degress of this vector */
-export function heading( v ) {
-  const deg = -Math.atan( v.x / v.y ) * 180 / Math.PI;
-  if ( v.x > 0 && v.y > 0 ) {
-    return deg + 180;
-  } else if ( v.x < 0 && v.y > 0 ) {
-    return deg + 180;
-  } else if ( v.x < 0 && v.y < 0 ) {
-    return deg + 360;
-  }
-  return deg;
-}
+Vector.add = (v1, v2) => ({
+  x: v1.x + v2.x,
+  y: v1.y + v2.y
+});
